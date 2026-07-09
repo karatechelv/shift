c53241aa9673e4de3d2f1e3f3847b0b8ed0218b8
karatechelv <keramati.m.pcp@gmail.com>
2026-07-10 02:11:21 +0330

    Create state.js

---
+++ frontend/supervisor/js/state.js
@@ -0,0 +1,27 @@
+export const state = {
+
+    supervisor:{
+
+        id:1,
+
+        name:"",
+
+        hospital:"",
+
+        department:"",
+
+        code:""
+
+    },
+
+    staffs:[],
+
+    schedules:[],
+
+    requests:[],
+
+    notifications:[],
+
+    rules:[]
+
+};
