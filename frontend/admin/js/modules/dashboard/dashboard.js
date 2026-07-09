c7a38f65f36f016e904bd320806b95a8ca13cca1
karatechelv <keramati.m.pcp@gmail.com>
2026-07-10 01:43:12 +0330

    Create dashboard.js

---
+++ frontend/admin/js/modules/dashboard/dashboard.js
@@ -0,0 +1,45 @@
+export function loadDashboard(){
+
+const app=document.getElementById("app");
+
+app.innerHTML=`
+
+<div class="cards">
+
+<div class="card">
+
+<h2>0</h2>
+
+<span>پرسنل</span>
+
+</div>
+
+<div class="card">
+
+<h2>0</h2>
+
+<span>شیفت امروز</span>
+
+</div>
+
+<div class="card">
+
+<h2>0</h2>
+
+<span>درخواست ها</span>
+
+</div>
+
+<div class="card">
+
+<h2>0</h2>
+
+<span>مرخصی</span>
+
+</div>
+
+</div>
+
+`;
+
+}
