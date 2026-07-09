6d85359e41cb43a13a3e43c3c6598ca22755a895
karatechelv <keramati.m.pcp@gmail.com>
2026-07-10 03:25:35 +0330

    Update app.js

--- frontend/supervisor/js/app.js
+++ frontend/supervisor/js/app.js
@@ -1,60 +1,7 @@
-import { initRouter } from "./router.js";
 import { state } from "./state.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+window.system = state;
 
-    initRouter();
+console.log("Karatech Shift Loaded");
 
-    loadSupervisor();
-
-    bindEvents();
-
-});
-
-function loadSupervisor() {
-
-    const info = document.getElementById("supervisorInfo");
-
-    info.innerHTML = `
-
-        <div class="info-card">
-
-            <div>
-                <strong>نام:</strong>
-                ${state.supervisor.name}
-            </div>
-
-            <div>
-                <strong>بیمارستان:</strong>
-                ${state.supervisor.hospital}
-            </div>
-
-            <div>
-                <strong>بخش:</strong>
-                ${state.supervisor.department}
-            </div>
-
-            <div>
-                <strong>شماره پرسنلی:</strong>
-                ${state.supervisor.code}
-            </div>
-
-        </div>
-
-    `;
-
-}
-
-function bindEvents(){
-
-    document
-        .getElementById("logoutBtn")
-        .addEventListener("click",()=>{
-
-            localStorage.removeItem("user");
-
-            location.href="../auth/login.html";
-
-        });
-
-}
+console.log(state);
