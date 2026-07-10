f44801e386418581e4780a16f42e77c1a29bbe34
karatechelv <keramati.m.pcp@gmail.com>
2026-07-10 13:07:50 +0330

    Create schedule.rules.js

---
+++ schedule.rules.js
@@ -0,0 +1,29 @@
+export const SHIFT = {
+
+    MORNING:"صبح",
+
+    EVENING:"عصر",
+
+    NIGHT:"شب",
+
+    OFF:"OFF",
+
+    LEAVE:"مرخصی"
+
+};
+
+export const RULES={
+
+    maxNight:8,
+
+    maxMorning:12,
+
+    maxEvening:12,
+
+    minOff:4,
+
+    noMorningAfterNight:true,
+
+    noNightAfterNight:true
+
+};
