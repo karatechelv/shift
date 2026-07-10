db742191743decb36f219418d8002d70b7f40955
karatechelv <keramati.m.pcp@gmail.com>
2026-07-10 23:32:33 +0330

    Update schedule.generator.js

--- schedule.generator.js
+++ schedule.generator.js
@@ -1,96 +1,75 @@
-import {ShiftRow} from "./schedule.model.js";
-import {SHIFT,RULES} from "./schedule.rules.js";
+import { SHIFT } from "./schedule.rules.js";
+import { canAssignShift } from "./schedule.constraints.js";
+import { getScore } from "./schedule.balance.js";
 
-const TYPES=[
-
-SHIFT.MORNING,
-
-SHIFT.EVENING,
-
-SHIFT.NIGHT,
-
-SHIFT.OFF
-
+const SHIFTS = [
+    SHIFT.MORNING,
+    SHIFT.EVENING,
+    SHIFT.NIGHT,
+    SHIFT.OFF
 ];
 
-export function generateSchedule(staffs,days){
+export function generate(staffs, days) {
 
-    const result=[];
+    staffs.forEach(staff => {
 
-    staffs.forEach(staff=>{
+        staff.shifts = [];
 
-        const row=new ShiftRow(staff);
+        staff.totalMorning = 0;
+        staff.totalEvening = 0;
+        staff.totalNight = 0;
+        staff.totalOff = 0;
 
-        let last=null;
+        for (let day = 1; day <= days; day++) {
 
-        for(let day=1;day<=days;day++){
+            let best = null;
+            let bestScore = -999999;
 
-            let shift=randomShift(last,row);
+            SHIFTS.forEach(shift => {
 
-            row.shifts.push(shift);
+                if (!canAssignShift(staff, day, shift))
+                    return;
 
-            updateCounter(row,shift);
+                const score = getScore(staff, shift);
 
-            last=shift;
+                if (score > bestScore) {
+
+                    bestScore = score;
+                    best = shift;
+
+                }
+
+            });
+
+            if (!best)
+                best = SHIFT.OFF;
+
+            staff.shifts.push(best);
+
+            switch (best) {
+
+                case SHIFT.MORNING:
+                    staff.totalMorning++;
+                    break;
+
+                case SHIFT.EVENING:
+                    staff.totalEvening++;
+                    break;
+
+                case SHIFT.NIGHT:
+                    staff.totalNight++;
+                    break;
+
+                case SHIFT.OFF:
+                    staff.totalOff++;
+                    break;
+
+            }
 
         }
 
-        result.push(row);
-
     });
 
-    return result;
-
-}
-
-function randomShift(last,row){
-
-    let available=[...TYPES];
-
-    if(RULES.noMorningAfterNight && last===SHIFT.NIGHT){
-
-        available=available.filter(x=>x!==SHIFT.MORNING);
-
-    }
-
-    if(RULES.noNightAfterNight && last===SHIFT.NIGHT){
-
-        available=available.filter(x=>x!==SHIFT.NIGHT);
-
-    }
-
-    return available[Math.floor(Math.random()*available.length)];
-
-}
-
-function updateCounter(row,shift){
-
-    switch(shift){
-
-        case SHIFT.MORNING:
-
-            row.totalMorning++;
-
-            break;
-
-        case SHIFT.EVENING:
-
-            row.totalEvening++;
-
-            break;
-
-        case SHIFT.NIGHT:
-
-            row.totalNight++;
-
-            break;
-
-        case SHIFT.OFF:
-
-            row.totalOff++;
-
-            break;
-
-    }
+    return staffs;
 
 }
