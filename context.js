6a391fc2cf242e73e820d430649bc35a2318f218
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:57:32 +0330

    Create context.js

---
+++ context.js
@@ -0,0 +1,27 @@
+export class ShiftContext{
+
+    constructor(){
+
+        this.hospital = null;
+
+        this.department = null;
+
+        this.staffs = [];
+
+        this.rules = [];
+
+        this.requests = [];
+
+        this.leave = [];
+
+        this.holidays = [];
+
+        this.settings = {};
+
+        this.year = 0;
+
+        this.month = 0;
+
+    }
+
+}
