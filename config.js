b1e2c4954515e6bb804b82bb23e856223030ab5f
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:43:19 +0330

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+const config={
+
+    appName:"Karatech ELV",
+
+    version:"2.0",
+
+    api:"",
+
+    debug:true
+
+};
+
+export async function loadConfig(){
+
+    window.APP_CONFIG=config;
+
+}
