87f5d9af1c2b3cc07b4c7be4a67bceb2e30155f3
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:12:35 +0330

    Create schedule.view.js

---
+++ modules/schedule/schedule.view.js
@@ -0,0 +1,37 @@
+export function createToolbar() {
+
+    return `
+
+<div class="toolbar">
+
+<button id="generate">
+
+تولید برنامه
+
+</button>
+
+<button id="saveSchedule">
+
+ذخیره
+
+</button>
+
+<button id="exportSchedule">
+
+خروجی
+
+</button>
+
+<button id="printSchedule">
+
+چاپ
+
+</button>
+
+</div>
+
+<div id="tableArea"></div>
+
+`;
+
+}
