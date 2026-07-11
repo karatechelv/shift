dbb2ede926a789673b12a7d05fcbb31c4ca32ce9
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:59:35 +0330

    Update BaseService.js

--- BaseService.js
+++ BaseService.js
@@ -1,1 +1,41 @@
+import { Repository } from "../database/repository.js";
 
+export class BaseService {
+
+    constructor(table){
+
+        this.repository = new Repository(table);
+
+    }
+
+    getAll(){
+
+        return this.repository.all();
+
+    }
+
+    get(id){
+
+        return this.repository.find(id);
+
+    }
+
+    create(item){
+
+        return this.repository.insert(item);
+
+    }
+
+    update(item){
+
+        return this.repository.update(item);
+
+    }
+
+    delete(id){
+
+        return this.repository.delete(id);
+
+    }
+
+}
