acc8a21aa7fa6c2c675274c1d49746842ba43b3a
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:45:21 +0330

    Create permissions.js

---
+++ shared/auth/permissions.js
@@ -0,0 +1,17 @@
+export const PERMISSIONS = {
+
+    MANAGE_SYSTEM: "manage_system",
+
+    MANAGE_STAFF: "manage_staff",
+
+    MANAGE_SCHEDULE: "manage_schedule",
+
+    APPROVE_REQUESTS: "approve_requests",
+
+    VIEW_REPORTS: "view_reports",
+
+    STAFF_REQUEST: "staff_request",
+
+    VIEW_SELF_SCHEDULE: "view_self_schedule"
+
+};
