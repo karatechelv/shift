c60574e2625e5dd31e70ae4db436492f9122808d
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 08:43:00 +0330

    Create state.js

---
+++ state.js
@@ -0,0 +1,21 @@
+class State {
+
+    constructor(){
+
+        this.user=null;
+
+        this.hospital=null;
+
+        this.department=null;
+
+        this.permissions=[];
+
+        this.settings={};
+
+        this.cache={};
+
+    }
+
+}
+
+export default new State();
