9dd487ffe4ab0618aac452ece6efc2ca4829d1a8
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 09:06:53 +0330

    Create defaultData.js

---
+++ shared/storage/defaultData.js
@@ -0,0 +1,29 @@
+export const defaultData = {
+
+    version: 1,
+
+    hospitals: [],
+
+    departments: [],
+
+    supervisors: [],
+
+    staffs: [],
+
+    schedules: [],
+
+    requests: [],
+
+    notifications: [],
+
+    settings: {
+
+        theme: "light",
+
+        language: "fa",
+
+        autoSave: true
+
+    }
+
+};
