af3d321091320682ba3c722e96bf943883c19610
karatechelv <keramati.m.pcp@gmail.com>
2026-07-11 09:01:31 +0330

    Create WorkflowContext.js

---
+++ WorkflowContext.js
@@ -0,0 +1,31 @@
+export class WorkflowContext{
+
+    constructor(){
+
+        this.user = null;
+
+        this.hospital = null;
+
+        this.department = null;
+
+        this.year = 0;
+
+        this.month = 0;
+
+        this.schedule = null;
+
+        this.staffs = [];
+
+        this.rules = [];
+
+        this.requests = [];
+
+        this.leave = [];
+
+        this.notifications = [];
+
+        this.logs = [];
+
+    }
+
+}
